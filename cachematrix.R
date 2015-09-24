5ab12dbd4fe37ec1548bb8eef65b95bb8a7cd711
MJornet <marta.jornet@gmail.com>
2015-09-24 09:09:23 +0200

    Programming Assignment 2
    Caching example

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculate the inverse of a matrix
+## cache the answer, in order not to do the same calculation twice
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Create the cache input and output matrices
+makeCacheMatrix <- function(inputMatrix = matrix()) {
+  #the result is initialized
+  inverseMatrix <- NULL
+  #Adding it to the cache
+  set <- function(y) {
+    inputMatrix <<- y
+    inverseMatrix <<- NULL
+  }
+  get <- function() inputMatrix
+  setsolve <- function(solve) inverseMatrix <<- solve
+  getsolve <- function() inverseMatrix
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#check if the matrix is cached to return the cached inverse, otherwise calculate the inverse and cache it
+cachesolve <- function(inputMatrix, ...) {
+  #If the matrix is cached, we already have the solution
+  inverseMatrix <- inputMatrix$getsolve()
+  if(!is.null(inverseMatrix)) {
+    message("getting cached data")
+    return(inverseMatrix)
+  }
+  #If it is not cached, we calculate it
+  data <- inputMatrix$get()
+  inverseMatrix <- solve(data, ...)
+  inputMatrix$setsolve(inverseMatrix)
+  inverseMatrix
 }
